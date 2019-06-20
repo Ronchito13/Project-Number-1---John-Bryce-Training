3a71fa9f05f75d1a8a9146614663a5a9197f5327
Ron Dayan <45286087+Ronchito13@users.noreply.github.com>
2019-06-20 10:10:31 +0300

    Update code.js

--- js/code.js
+++ js/code.js
@@ -189,3 +189,19 @@ function resetForm(){
 }
 
 
+// get Today Date
+
+
+function getToday(){
+    
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+
+return today;
+}
+
+
